9260de74c2014addc17c4064166b258e80b3e98e
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-05-24 00:03:27 -0300

    ejercicio 39

---
+++ CAIEJR39.A.L.D/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIEJR39.A.L.D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Solicite el ingreso de una serie de números al usuario y los presente en orden inverso.
+
+            int[] arraynumerosIngreso;
+
+            arraynumerosIngreso = new int[10];
+
+            for (int indice = 0; indice < arraynumerosIngreso.Length; indice++)
+            {
+                Console.Write($"Ingrese un número para la posición {indice}: ");
+                bool ok = false;
+
+                while (!ok)
+                {
+                    string ingreso = Console.ReadLine();
+                    ok = int.TryParse(ingreso, out arraynumerosIngreso[indice]);
+
+                    if (!ok)
+                    {
+                        Console.WriteLine("No ingreso un número válido.");
+                    }
+
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Ingreso de forma inversa: ");
+            System.Array.Reverse(arraynumerosIngreso);
+            foreach (int valor in arraynumerosIngreso)
+            {
+                Console.WriteLine(valor);
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    
+    }
+}
