cd3d72243a53d1d31eec19395314c28f9c3cd37f
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-05-24 10:26:13 -0300

    ejercicio 42 (no funciona)

---
+++ CAIEJR42.A.L.D/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIEJR42.A.L.D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite el ingreso de una palabra al usuario y la presente escrita al revés.
+
+            List<string> lstString = new List<string>();
+
+            string ingreso = "";
+            bool ok = false;
+            while(!ok)
+            {
+                Console.Write($"Ingrese una palabra para la posición {lstString.Count}: ");
+                ingreso = Console.ReadLine();
+                ok = string.IsNullOrEmpty(ingreso);
+
+                if(ok)
+                {
+                    Console.WriteLine("Debe ingresar una palabra.");
+                }
+                else
+                {
+                    lstString.Add(ingreso);
+                    break;
+                }
+
+            }
+
+            lstString.Reverse();
+            foreach (string str in lstString)
+            {
+                Console.WriteLine(str);
+            }
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
